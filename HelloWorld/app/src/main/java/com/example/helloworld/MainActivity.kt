527ea6135a9d112c3d02f5d680fe59285a594835
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-02 09:44:04 -0300

    create binding and changeable text

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // Binding is the root of the aplication
+        setContentView(binding.root)
+
+        binding.buttonHello.setOnClickListener{/* Creates a event that is listening for
+        the clicks on the button */
+            val name : String = binding.editName.text.toString()
+            binding.textView.text = "Hello " + name
+        }
     }
 }
