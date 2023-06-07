d5605c8d7afac8634c8e1681ba772c1796a6149b
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-06-07 00:01:44 -0300

    add initial login page with validation

---
+++ LoginPage/app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.loginpage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.emailField.text.toString()
+            val password = binding.passwordField.text.toString()
+
+            val emailError = validateEmail(email)
+            val passwordError = validatePassword(password)
+
+            if (emailError != null) {
+                binding.emailField.error = emailError
+            } else if (passwordError != null) {
+                binding.passwordField.error = passwordError
+            }
+            else {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateEmail(email: String): String? {
+        return if (email.isEmpty()) {
+            "Email Field cannot be empty"
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            "Invalid email address"
+        } else if (!isValidDomain(email.substringAfter("@"))) {
+            "Invalid domain"
+        } else {
+            null
+        }
+    }
+
+    private fun isValidDomain(domain: String): Boolean {
+        return domain.endsWith("byteserido.com") || domain.endsWith("ufrn.edu.br")
+    }
+
+    private fun validatePassword(password: String): String? {
+        return when {
+            password.isEmpty() -> "Password Field cannot be empty"
+            password.length < 4 -> "Password must be at least 4 characters long"
+            !password.matches(Regex(".*[a-z].*")) || !password.matches(Regex(".*[A-Z].*")) ->
+                "Password must contain at least one uppercase and one lowercase letter"
+            else -> null
+        }
+    }
+}
+
